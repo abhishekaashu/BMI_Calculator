dcd5a8a001aa9cc48160a90faffe74f9c154907a
Abhishek Singh <42812585+abhishekaashu@users.noreply.github.com>
2020-05-01 10:36:39 +0530

    Add files via upload

---
+++ lib/results_page.dart
@@ -0,0 +1,73 @@
+import 'package:bmi_calculator/const.dart';
+import 'package:bmi_calculator/reusable_card.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'const.dart';
+import 'buttom_button.dart';
+
+class ResultsPage extends StatelessWidget {
+  ResultsPage({@required this.bmiResult, @required this.resultText,@required this.interpretation});
+
+  final String bmiResult;
+  final String resultText;
+  final String interpretation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                'Your Result',
+                style: kTitleTextStyle,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    resultText.toUpperCase(),
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    bmiResult,
+                    style: kBMITextStyle,
+                  ),
+                  Text(
+                    'Normal BMI Range:''\n'
+                        '18.5-25 kg/m2',
+                    style: kResultTextStyle,
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    interpretation,
+                    style: kBodyTextStyle,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          BottomButtom(onTap: (){
+            Navigator.pop(context);
+          }, buttomTitle: 'RE-CALCULATE'),
+        ],
+      ),
+    );
+  }
+}
